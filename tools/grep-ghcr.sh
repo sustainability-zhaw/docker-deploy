bbca7a33bcec87e41cbb86a56892f81c7eed2a1a
Christian Glahn <cpglahn@gmail.com>
2023-01-14 12:31:06 +0100

    preload the latest container SHA from GH, fixes #1

---
+++ tools/grep-ghcr.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "containers:" > configs/containers.yaml
+echo -n "  dashboard_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/sustainability-dashboard/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  adresolver_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/ad-resolver/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  schema_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/dgraph-schema/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  authomator_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/phish108/authomator/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  dspace_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/extraction-dspace/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  evento_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/extraction-evento/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  sdgindexer_sha: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/sdg-indexer/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
