448218d5b626bffa6d14cad24f4124195422fd85
Christian Glahn <cpglahn@gmail.com>
2023-02-02 07:56:45 +0100

    one off keyword deployment

--- tools/grep-ghcr.sh
+++ tools/grep-ghcr.sh
@@ -15,3 +15,5 @@ echo -n "  evento_sha: " >> configs/containers.yaml
 curl -s https://api.github.com/repos/sustainability-zhaw/extraction-evento/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
 echo -n "  sdgindexer_sha: " >> configs/containers.yaml
 curl -s https://api.github.com/repos/sustainability-zhaw/sdg-indexer/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
+echo -n "  keywords: " >> configs/containers.yaml
+curl -s https://api.github.com/repos/sustainability-zhaw/keywords/git/refs | jq -r '.[] | select(.ref == "refs/heads/main") | .object.sha[0:7]' >> configs/containers.yaml
